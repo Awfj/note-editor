f502abe31760c48aae5d72887c1598c9ffd6c895
Alexey Kokhanovsky <awfj837@gmail.com>
2019-06-16 17:40:56 +0300

    Updates

---
+++ precache-manifest.7406ee20c1e29a7d7986d089efed4dc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e0830521251f9fd1df367dcd6003e80",
+    "url": "/note-editor/index.html"
+  },
+  {
+    "revision": "4b9980f428f3c90698c1",
+    "url": "/note-editor/static/css/main.385f2b1e.chunk.css"
+  },
+  {
+    "revision": "f796ffc7eed2bbc0189b",
+    "url": "/note-editor/static/js/2.bdf50ed4.chunk.js"
+  },
+  {
+    "revision": "4b9980f428f3c90698c1",
+    "url": "/note-editor/static/js/main.e8428425.chunk.js"
+  },
+  {
+    "revision": "ca858a6c03e2b40549ff",
+    "url": "/note-editor/static/js/runtime~main.3e3022e0.js"
+  }
+]);
